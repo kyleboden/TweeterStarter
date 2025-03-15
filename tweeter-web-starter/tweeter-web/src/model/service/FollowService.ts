b148c96aa3c0cdf3abbbdd4b5ded4d59e8b29126
Kyle <bodenkyle8@gmail.com>
2025-03-15 09:31:48 -0600

    m3 getfollowers but still infinite scroll

--- tweeter-web-starter/tweeter-web/src/model/service/FollowService.ts
+++ tweeter-web-starter/tweeter-web/src/model/service/FollowService.ts
@@ -14,7 +14,15 @@ export class FollowService {
     lastItem: User | null
   ): Promise<[User[], boolean]> {
     // TODO: Replace with the result of calling server
-    return FakeData.instance.getPageOfUsers(lastItem, pageSize, userAlias);
+    // return FakeData.instance.getPageOfUsers(lastItem, pageSize, userAlias);
+    const facade = new ServerFacade();
+    const request: PagedUserItemRequest = {
+      token: authToken.token,
+      userAlias: userAlias,
+      pageSize: pageSize,
+      lastItem: lastItem,
+    };
+    return facade.getMoreFollowers(request);
   }
 
   public async loadMoreFollowees(
@@ -25,14 +33,12 @@ export class FollowService {
   ): Promise<[User[], boolean]> {
     // TODO: Replace with the result of calling server
     // return FakeData.instance.getPageOfUsers(lastItem, pageSize, userAlias);
-    // create an instance of server facade
-    // facade.getMoreFollowees.
     const facade = new ServerFacade();
     const request: PagedUserItemRequest = {
       token: authToken.token,
       userAlias: userAlias,
       pageSize: pageSize,
-      lastItem: lastItem
+      lastItem: lastItem,
     };
     return facade.getMoreFollowees(request);
   }
